8b9efc874924fa9a01b4d8b19bff3ce07dddad95
Donovan Cotter <donovan.cotter@yahoo.com>
2015-10-27 15:14:25 -0400

    updated model with class of Winery and has a func to get the Google Places info from a winery Instance

--- TCWineTour/Winery.swift
+++ TCWineTour/Winery.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import GoogleMaps
+
+//let chateauChantalmarker = GMSMarker()
+//let twoLads = GMSMarker()
+//let marker = GMSMarker()
+
+class Winery {
+    var position = CLLocationCoordinate2D()
+    var title = String()
+    var marker = GMSMarker()
+
+
+    init(position: CLLocationCoordinate2D, title: String, marker: GMSMarker) {
+        self.position = position
+        self.title = title
+        self.marker =
+    }
+    
+    
+}
+
+//chateauChantalmarker.position = CLLocationCoordinate2DMake(44.865389, -85.520597)
+//chateauChantalmarker.title = "Chateau Chantal"
+//chateauChantalmarker.snippet = "Winery"
+//
+//twoLads.position = CLLocationCoordinate2DMake(44.933800, -85.493137)
+//twoLads.title = "2 Lads"
+//twoLads.snippet = "Winery"
+//twoLads.map = mapView
