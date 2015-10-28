60be15ab1ba570ebd14c467e355a4aa0b254b382
Donovan Cotter <donovan.cotter@yahoo.com>
2015-10-28 16:06:11 -0400

    added xib and infoWindowVC

---
+++ TCWineTour/InfoWindowVC.swift
@@ -0,0 +1,34 @@
+//
+//  InfoWindowVC.swift
+//  TCWineTour
+//
+//  Created by Donovan Cotter on 10/28/15.
+//  Copyright © 2015 DonovanCotter. All rights reserved.
+//
+
+import UIKit
+
+class InfoWindowVC: UIViewController {
+    @IBOutlet weak var infoWindowLabel: UILabel!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func infoButton(sender: UIButton) {
+        
+    }
+    
+
+}
